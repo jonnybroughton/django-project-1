d3a7eec624d6cf32a8f46bf701fcccf269fc9f28
Jonny Broughton <jonny.broughton08@gmail.com>
2024-09-09 14:53:45 +0000

    add gunicorn to requirements and add procfile

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    if request.method == 'POST':
-        return HttpResponse('You must have POSTed something')
+
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
     else:
         return HttpResponse(request.method)
